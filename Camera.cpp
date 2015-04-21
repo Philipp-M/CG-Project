8d4d97f975de9d6f6a6400b90c0802b19fdd7fde
Philipp Mildenberger <philipp.mildenberger@student.uibk.ac.at>
2015-04-21 21:34:03 +0200

    finished the basic camera

--- Camera.cpp
+++ Camera.cpp
@@ -1,23 +1,79 @@
 #include "Camera.hpp"
 #include <cmath>
 #include <iostream>
+#include <glm/ext.hpp>
 
-Camera::Camera(GLfloat width, GLfloat height, GLfloat clipNear, GLfloat clipFar, GLfloat fieldOfView) :
+Camera::Camera(GLfloat width, GLfloat height, GLfloat nearPlane, GLfloat farPlane, GLfloat fieldOfView) :
         width(width),
         height(height),
-        clipNear(clipNear),
-        clipFar(clipFar),
+        nearPlane(nearPlane),
+        farPlane(farPlane),
         fieldOfView(fieldOfView),
-        transMatrix(glm::mat4x4(0.0))
 {
-    setUpTransMatrix();
+//    setUpTransMatrix();
 }
 
 void Camera::setUpTransMatrix()
 {
-    transMatrix[0][0] = 1.0 / (tan(fieldOfView / 2.0) * ((double) height / (double) width));
-    transMatrix[1][1] = 1.0 / tan(fieldOfView / 2.0);
-    transMatrix[2][2] = -(clipFar + clipNear) / (clipFar - clipNear);
-    transMatrix[2][3] = -1.0;
-    transMatrix[3][2] = -2.0 * clipFar * clipNear / (clipFar - clipNear);
+    /*transMatrix[0][0] = (GLfloat) (1.0 / (tan(fieldOfView / 2.0) * ((double) height / (double) width)));
+    transMatrix[1][1] = (GLfloat) (1.0 / tan(fieldOfView / 2.0));
+    transMatrix[2][2] = -(farPlane + nearPlane) / (farPlane - nearPlane);
+    transMatrix[2][3] = (GLfloat) -1.0;
+    transMatrix[3][2] = (GLfloat) (-2.0 * farPlane * nearPlane / (farPlane - nearPlane));*/
+}
+
+glm::vec3 Camera::getPosition()
+{
+    return position;
+}
+
+void Camera::setPosition(glm::vec3 position)
+{
+    this->position = position;
+}
+void Camera::move(glm::vec3 delta)
+{
+    this->position += delta;
+}
+glm::mat4 Camera::getTransformationMatrix()
+{
+    glm::mat4 retMat = glm::perspective(fieldOfView, width / height, nearPlane, farPlane);
+
+    retMat *= glm::rotate(rotX, glm::vec3(1, 0, 0)) * glm::rotate(rotX, glm::vec3(0, 1, 0)) *
+              glm::rotate(rotX, glm::vec3(0, 0, 1));
+    retMat *= glm::translate(glm::vec3(position.x, position.y, position.z));
+    return retMat;
+}
+
+glm::vec3 Camera::getRot()
+{
+    return glm::vec3(rotX, rotY, rotZ);
+}
+
+GLfloat Camera::getRotX()
+{
+    return rotX;
+}
+
+GLfloat Camera::getRotY()
+{
+    return rotY;
+}
+
+GLfloat Camera::getRotZ()
+{
+    return rotZ;
+}
+
+void Camera::setRotation(GLfloat rotX, GLfloat rotY, GLfloat rotZ)
+{
+    this->rotX = rotX;
+    this->rotY = rotY;
+    this->rotZ = rotZ;
+}
+void Camera::rotate(GLfloat rotX, GLfloat rotY, GLfloat rotZ)
+{
+    this->rotX += rotX;
+    this->rotY += rotY;
+    this->rotZ += rotZ;
 }
--- Camera.hpp
+++ Camera.hpp
@@ -8,10 +8,23 @@ private:
     GLfloat fieldOfView;
     GLfloat width;
     GLfloat height;
-    GLfloat clipNear;
-    GLfloat clipFar;
-    glm::mat4x4 transMatrix;
+    GLfloat nearPlane;
+    GLfloat farPlane;
+    glm::vec3 position;
+    GLfloat rotX;
+    GLfloat rotY;
+    GLfloat rotZ;
     void setUpTransMatrix();
 public:
     Camera(GLfloat width, GLfloat height, GLfloat clipNear, GLfloat clipFar, GLfloat fieldOfView);
+    void setPosition(glm::vec3 position);
+    void move(glm::vec3 delta);
+    glm::vec3 getPosition();
+    glm::vec3 getRot();
+    GLfloat getRotX();
+    GLfloat getRotY();
+    GLfloat getRotZ();
+    void setRotation(GLfloat rotX, GLfloat rotY, GLfloat rotZ);
+    void rotate(GLfloat rotX, GLfloat rotY, GLfloat rotZ);
+    glm::mat4 getTransformationMatrix();
 };
